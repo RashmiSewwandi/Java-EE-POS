c91634282ae23d0f665c293c4140fd009372d163
Rashmi Sewwandi <rashmiswa09@gmail.com>
2022-05-27 19:16:59 +0530

    customer update

--- src/servlet/customerServlet.java
+++ src/servlet/customerServlet.java
@@ -2,9 +2,7 @@ package servlet;
 
 
 import javax.annotation.Resource;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -180,6 +178,62 @@ public class customerServlet extends HttpServlet {
             }
         }
 
+
+
     }
 
+    //update
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+
+        String id = jsonObject.getString("id");
+        String name = jsonObject.getString("name");
+        String address = jsonObject.getString("address");
+        String salary = jsonObject.getString("salary");
+
+        PrintWriter writer = resp.getWriter();
+
+        Connection connection =null;
+        try {
+
+            connection = ds.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("UPDATE customer SET name=?,address=?,salary=? WHERE id=?");
+            pstm.setObject(1,name);
+            pstm.setObject(2,address);
+            pstm.setObject(3,salary);
+            pstm.setObject(4,id);
+
+            if(pstm.executeUpdate()>0){
+                JsonObjectBuilder response = Json.createObjectBuilder();//{"data":[],"massage":"Done","status":200}
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+                response.add("status",200);
+                response.add("message","successfully updated");
+                response.add("data","");
+
+                writer.print(response.build());
+            }
+
+        } catch (SQLException throwables) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("status",400);
+            response.add("message","error");
+            response.add("data",throwables.getLocalizedMessage());
+
+            writer.print(response.build());
+            resp.setStatus(HttpServletResponse.SC_OK);
+
+        }
+        finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+
+    }
 }
